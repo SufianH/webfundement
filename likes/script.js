0019835f1561f3c2d1f2952dfa2825bd948c880d
SufianH <igotflaws@outlook.com>
2021-09-22 02:38:55 -0400

    commit

---
+++ likes/script.js
@@ -0,0 +1,11 @@
+function addtolikes(){
+    document.querySelector("#num1").innerText++;
+}
+
+function addtolikes2(){
+    document.querySelector("#num2").innerText++;
+}
+
+function addtolikes3(){
+    document.querySelector("#num3").innerText++;
+}
